5a9e1164679b91c025ed7793a102f639826735fe
maivy111205 <maithithaovy632@gmail.com>
2026-06-16 19:26:05 +0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -2,3 +2,10 @@ document.addEventListener('DOMContentLoaded', () => {
     console.log('Portfolio của Mai Thị Thảo Vy đã load xong!');
     alert('Chào mừng bạn đến với portfolio thật trên Internet của mình!');
 });
+const btn=document.getElementById("themeBtn");
+
+if(btn){
+    btn.addEventListener("click",()=>{
+        document.body.classList.toggle("dark");
+    });
+}
