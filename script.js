027d73aac72fe6cb56d6b47d9b066f2b0bc00361
maivy111205 <maithithaovy632@gmail.com>
2026-06-16 18:06:05 +0700

    Delete script.js

--- script.js
+++
@@ -1,4 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    console.log('Portfolio của Mai Thị Thảo Vy đã load xong!');
-    alert('Chào mừng bạn đến với portfolio thật trên Internet của mình!');
-});
