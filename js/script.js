1ace63c435bd7c3ff441d3690b42779a5d189c1f
maivy111205 <maithithaovy632@gmail.com>
2026-06-16 23:51:08 +0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,22 +1,29 @@
 document.addEventListener('DOMContentLoaded', () => {
+
+    if(!localStorage.getItem("welcomeShown")){
+        alert("Chào mừng bạn đến với portfolio thật trên Internet của mình!");
+        localStorage.setItem("welcomeShown","true");
+    }
+
     console.log('Portfolio của Mai Thị Thảo Vy đã load xong!');
-    alert('Chào mừng bạn đến với portfolio thật trên Internet của mình!');
 });
-const btn=document.getElementById("themeBtn");
+
+const btn = document.getElementById("themeBtn");
 
 if(btn){
-    btn.addEventListener("click",()=>{
+    btn.addEventListener("click", () => {
         document.body.classList.toggle("dark");
     });
 }
-const observer = new IntersectionObserver(entries=>{
-    entries.forEach(entry=>{
+
+const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
         if(entry.isIntersecting){
             entry.target.classList.add("show");
         }
     });
 });
 
-document.querySelectorAll(".fade").forEach(el=>{
+document.querySelectorAll(".fade").forEach(el => {
     observer.observe(el);
 });
