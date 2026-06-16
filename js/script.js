01e9d76c8fdc5aa05924d11e4e7a6cbbae6cd9dd
maivy111205 <maithithaovy632@gmail.com>
2026-06-16 22:30:02 +0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -8,3 +8,14 @@ if(btn){
         document.body.classList.toggle("dark");
     });
 }
+const observer = new IntersectionObserver(entries=>{
+    entries.forEach(entry=>{
+        if(entry.isIntersecting){
+            entry.target.classList.add("show");
+        }
+    });
+});
+
+document.querySelectorAll(".fade").forEach(el=>{
+    observer.observe(el);
+});
