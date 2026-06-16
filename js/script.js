8d81c5d3d2c5f862c42d34eff63a31295d1a0a4a
maivy111205 <maithithaovy632@gmail.com>
2026-06-16 23:41:42 +0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,22 +1,9 @@
-document.addEventListener('DOMContentLoaded', () => {
-    console.log('Portfolio của Mai Thị Thảo Vy đã load xong!');
-    alert('Chào mừng bạn đến với portfolio thật trên Internet của mình!');
-});
-const btn=document.getElementById("themeBtn");
+alert("Chào mừng bạn đến với portfolio thật trên Internet của mình!");
+
+const btn = document.getElementById("themeBtn");
 
 if(btn){
-    btn.addEventListener("click",()=>{
+    btn.addEventListener("click", () => {
         document.body.classList.toggle("dark");
     });
 }
-const observer = new IntersectionObserver(entries=>{
-    entries.forEach(entry=>{
-        if(entry.isIntersecting){
-            entry.target.classList.add("show");
-        }
-    });
-});
-
-document.querySelectorAll(".fade").forEach(el=>{
-    observer.observe(el);
-});
